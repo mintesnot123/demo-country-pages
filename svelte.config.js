24f5970828c07f60b46741bbac3a1d82c1844934
mintesnot123 <yismawmintesnot@gmail.com>
2023-05-23 15:48:21 -0700

    updated svelte config file to support scss

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,11 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		scss: {
+			prependData: `@import '/static/assets/sass/country/country-pages.scss';`
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
