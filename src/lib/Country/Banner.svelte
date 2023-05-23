30f566e50dce223962c47d17a4267122ae1eb658
mintesnot123 <yismawmintesnot@gmail.com>
2023-05-23 15:51:19 -0700

    implemented Banner, FloatingMenu and UserRating components

---
+++ src/lib/Country/Banner.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	export let H1Text: string;
+	export let H2Text: string;
+	export let BackgroundImageUrl: string;
+	export let MobileBackgroundImageUrl: string;
+	export let ButtonText: string;
+	export let ButtonTitle: string;
+	export let ButtonUrl: string;
+</script>
+
+<section
+	id="banner"
+	class="page-banner HC_text-align-center"
+	style="background-image: url('{BackgroundImageUrl}')"
+>
+	<div style="margin: 0px auto; max-width: 1200px " id="homeBanner">
+		<div class="row">
+			<div
+				class="col-md-11 col-md-offset-1 col-sm-11 col-sm-offset-1 col-xs-10 offset-md-1 offset-sm-1 offset-1"
+			>
+				<h1 class="VP">{@html H1Text}</h1>
+				<h2 class="VP2">{@html H2Text}</h2>
+			</div>
+		</div>
+		<div class="row" style="display: block; flex-wrap: nowrap">
+			<div style="height: 60px;">&nbsp;</div>
+			<div class="col-md-offset-1 col-sm-offset-1 col-xs-offset-1 offset-md-1 offset-sm-1 offset-1">
+				{#if ButtonUrl}
+					<a href={ButtonUrl.toLowerCase()} title={ButtonTitle} class="homeMainGetEstimate"
+						>{ButtonText}</a
+					>
+				{/if}
+			</div>
+			<br />
+		</div>
+	</div>
+</section>
+
+<style lang="scss">
+	@media (max-width: 800px) {
+		.page-banner {
+			background-image: url('{MobileBackgroundImageUrl}') !important;
+		}
+	}
+</style>
---
+++ src/lib/Country/UserRating.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	// export let DivLine1: string;
+	// export let DivLine2: string;
+</script>
+
+<div class="banner-bottom">
+	<div class="reviews">
+		<div class="maxWidth row">
+			<div>
+				<span class="d-none d-sm-inline hidden-xs">Secureship </span>Rating:
+				<span id="rating" data-rating="4.9">4.9</span>
+				<span class="star-rating" id="starRating">
+					<i class="fas fa-star" />
+					<i class="fas fa-star" />
+					<i class="fas fa-star" />
+					<i class="fas fa-star" />
+					<i class="fas fa-star" />
+				</span>
+				<a
+					title="Secureship Reviews and Testimonials by Google Users"
+					target="_blank"
+					rel="nofollow noreferrer"
+					href="https://www.google.com/search?q=secureship&oq=secureship&aqs=chrome..69i57j69i60l5.1767j0j7&sourceid=chrome&ie=UTF-8#lrd=0x4cce08a58efcbc3d:0xf0163d6908f77cb1,1,,,"
+				>
+					<span id="reviews">100</span>
+					reviews</a
+				>
+			</div>
+			<div>
+				<img
+					class="carrier-logo"
+					src="/images/carriers/purolator-transparent.png"
+					alt="Get Purolator discounted shipping for your business"
+					title="Get Purolator discounted shipping for your business"
+				/>
+				<img
+					class="carrier-logo"
+					src="/images/carriers/ups-transparent.png"
+					alt="Cheap shipping through UPS"
+					title="Cheap shipping through UPS"
+				/>
+				<img
+					class="carrier-logo"
+					src="/images/carriers/fedex-transparent.png"
+					alt="Discounted shipping prices with FedEx"
+					title="Discounted shipping prices with FedEx"
+				/>
+				<div class="visible-xs d-sm-none" style="height: 1px;">
+					<br />
+				</div>
+				<img
+					class="carrier-logo"
+					src="/images/carriers/canpar-transparent.png"
+					alt="Best and discounted pricing with Canpar"
+					title="Best and discounted pricing with Canpar"
+				/>
+				<img
+					class="carrier-logo"
+					src="/images/carriers/canadapost-transparent.png"
+					alt="Use your Canada Post account through Secureship"
+					title="Use your Canada Post account through Secureship"
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="shipping-carriers" />
+</div>
+
+<style lang="scss">
+	.carrier-logo {
+		margin: 0px 5px;
+		width: auto;
+		height: 40px;
+	}
+
+	.reviews,
+	.review-content {
+		text-align: center;
+	}
+
+	.carrier-logo-section {
+		text-align: center;
+	}
+
+	@media (max-width: 600px) {
+		.carrier-logo {
+			height: 40px;
+		}
+	}
+</style>
